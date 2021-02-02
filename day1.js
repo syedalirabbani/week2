7e21d38d701f5376bf31c75af968dc9b563274e8
syedalirabbani <77966033+syedalirabbani@users.noreply.github.com>
2021-02-02 15:51:12 +0800

    Add files via upload

--- day1.js
+++ day1.js
@@ -60,7 +60,7 @@ if ( age<18){
   console.log("you cannot watch the movie");
 }
 else{
-  consloe.log("you can watch the movie");
+  console.log("you can watch the movie");
 }
 
 if(age < 13){
@@ -135,6 +135,35 @@ for (var i = 1; i < lines; i++){
   console.log(i+" x " + multiplier + " = " + multiplier*i);
 }
 
+for (var i = 1; i<101; i++){
+  if( i % 3 ==0){
+    console.log("Fizz")
+  }
+  else if( i % 5 == 0){
+    console.log("Buzz")
+  }
+  else if( i % 3 == 0 && i % 5 == 0){
+    console.log("FizzBuzz")
+  }
+  else{
+    console.log(i)
+  }
+}
+  
+ 
+  
+    
+
+  
+ 
+  
+
+  
+  
+  
+
+  
+  
 
 
 
